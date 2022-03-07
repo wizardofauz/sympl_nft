060127cadff33b3a6cfc56f5714381225060f65d
Aurelien Bonnel <aurelien.bonnel@gmail.com>
2022-03-07 12:56:31 +0100

    test(max_mint): Added tests for max minting

--- test/nft_test.py
+++ test/nft_test.py
@@ -21,14 +21,14 @@ class TestNFT():
     def test_nft_life_cycle(self, nft):
         assert len(nft.get_nfts()) == 0
 
-        new_nft = nft.create_nft(name='SymNyan', symbol='~=[,,_,,]:3', owner_address='eth://sendmemoney')
+        new_nft = nft.create_nft(name='SymNyan', symbol='~=[,,_,,]:3', owner_address='eth://sendmemoney', max_mint=None)
 
         assert new_nft['name'] == 'SymNyan'
         assert len(nft.get_nfts()) == 1
         nft.creator_of(id=new_nft['id'])['address'] == 'eth://sendmemoney'
 
     def test_can_mint(self, nft):
-        new_nft = nft.create_nft(name='SymNyan v2', symbol='~=[,,,_,,,]:3', owner_address='eth://sendmemoney')
+        new_nft = nft.create_nft(name='SymNyan v2', symbol='~=[,,,_,,,]:3', owner_address='eth://sendmemoney', max_mint=None)
 
         nft.creator_of(id=new_nft['id'])['address'] == 'eth://sendmemoney'
         retrieved_nft = nft.get_nft(id=new_nft['id'])
@@ -38,8 +38,21 @@ class TestNFT():
         reretrieved_nft = nft.get_nft(id=new_nft['id'])
         assert reretrieved_nft['balances']['eth://sendmemoney'] == '2'
 
+    def test_cannot_overmint(self, nft):
+        new_nft = nft.create_nft(name='SymNyan v2.5', symbol='~=[,,,__,,,]:3', owner_address='eth://sendmemoney', max_mint=1)
+
+        nft.creator_of(id=new_nft['id'])['address'] == 'eth://sendmemoney'
+        retrieved_nft = nft.get_nft(id=new_nft['id'])
+        assert retrieved_nft['balances']['eth://sendmemoney'] == '1'
+
+        with pytest.raises(Exception):
+            nft.mint(to='eth://sendmemoney', nft_id=new_nft['id'])
+
+        reretrieved_nft = nft.get_nft(id=new_nft['id'])
+        assert reretrieved_nft['balances']['eth://sendmemoney'] == '1'
+
     def test_can_burn(self, nft):
-        new_nft = nft.create_nft(name='SymNyan v3', symbol='~=[,,,,_,,,,]:3', owner_address='eth://sendmemoney')
+        new_nft = nft.create_nft(name='SymNyan v3', symbol='~=[,,,,_,,,,]:3', owner_address='eth://sendmemoney', max_mint=None)
 
         nft.creator_of(id=new_nft['id'])['address'] == 'eth://sendmemoney'
         retrieved_nft = nft.get_nft(id=new_nft['id'])
@@ -50,7 +63,7 @@ class TestNFT():
         assert reretrieved_nft['balances']['eth://sendmemoney'] == '0'
 
     def test_cannot_overburn(self, nft):
-        new_nft = nft.create_nft(name='SymNyan v4', symbol='~=[,,,,,_,,,,,]:3', owner_address='eth://sendmemoney')
+        new_nft = nft.create_nft(name='SymNyan v4', symbol='~=[,,,,,_,,,,,]:3', owner_address='eth://sendmemoney', max_mint=None)
 
         nft.creator_of(id=new_nft['id'])['address'] == 'eth://sendmemoney'
         retrieved_nft = nft.get_nft(id=new_nft['id'])
